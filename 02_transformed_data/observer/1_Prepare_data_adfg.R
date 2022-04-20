19568818eb5a9c0a4086d1eb3745e51f8fae2acd
Max Olmos <olmosmaxim@gmail.com>
2022-04-19 17:03:11 -0700

    Update 1_Prepare_data_adfg.R

--- 02_transformed_data/observer/1_Prepare_data_adfg.R
+++ 02_transformed_data/observer/1_Prepare_data_adfg.R
@@ -77,9 +77,75 @@ Data_NA <- Data_0 %>% filter(is.na(lat))
 (unique(Data_NA$year))
 Data_NA %>% filter(year == 2015)
 
+
 # - Define CPUE -----------------------------------------------------------
 Data <- Data_0 %>% mutate(CPUE = yfswt/towDur )
 
+
+# plot spatial 
+
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+# EBS
+shapefile <- st_read("02_transformed_data/EBSshelf/EBSshelf.shp")
+EBS <- st_transform(shapefile,crs=crs_ref)
+
+# Connect to a PostGIS instance
+drv <- dbDriver("PostgreSQL")
+liaison <- dbConnect(drv, host="sirs.agrocampus-ouest.fr",
+                     user="atlas", password="atlas", dbname="world_mapping")
+
+crs_ref <- "+proj=longlat +ellps=WGS84  +datum=WGS84 +no_defs"
+grid_cell<-raster(ext=extent(-180,-150,49,77),res=c(1,0.5), crs = "+init=epsg:4326") %>%
+  rasterToPolygons() %>% # tranform en raster
+  st_as_sf() %>% # en sf
+  dplyr::rename(cell_no=layer) # cell number
+
+# we add the coordinate of the centroid and the cell number
+grid_cell<-grid_cell%>%
+  bind_cols(as.data.frame(st_coordinates(st_centroid(grid_cell$geometry))))%>% # coordonnees du centroide de la cellule
+  mutate(cell_no = seq(1:nrow(grid_cell)))%>%
+  dplyr::rename(lon_centr=X,
+                lat_centr=Y)
+
+crs_ref <- "+proj=longlat +ellps=WGS84  +datum=WGS84 +no_defs"
+grid_cell <- st_transform(grid_cell,crs=crs_ref)
+
+g <- ggplot()+
+  # color=NA pas de ocntour
+  # trait de cote
+  geom_sf(data = world,fill="black",color=NA) + 
+  
+  geom_sf(data=grid_cell, aes(fill=cell_no),color="black", fill=NA)+
+  coord_sf(xlim=c(-180,-155), ylim=c(54,63.5),expand = FALSE) 
+g
+
+
+p <- ggplot()+
+  geom_sf(data=grid_cell, aes(fill=cell_no),color="grey", fill=NA)+
+  geom_sf(data = world,fill="black",color=NA) +
+  geom_sf(data = EBS,fill="NA",color="brown",size=1) +
+  coord_sf(xlim=c(-180,-155), ylim=c(54,63.5),expand = FALSE) +   theme_bw() +
+  geom_point(data=Data,aes(x=long,y=lat),size=2,color="orange")
+
+p
+
+# -------------------------------------------------------------------------
+
+plot(log(Data_0$towDur),log(Data_0$yfswt), xlab='log(effort)', ylab="log(catch)")
+model <- lm(log(Data_0$yfswt)~log(Data_0$towDur))
+model
+abline(model)
+
+
+p <- ggplot(Data_0, aes(x = log(Data_0$towDur), y = log(Data_0$yfswt))) + 
+  geom_point() +
+  stat_smooth(method = "lm", col = "red") +   theme_bw() + xlab("log(effort)")+ ylab("log(catch)")+
+  annotate("text", x = 0, y=7, label = "log(catch) =  -1.335 + 1.247*log(effort)",size=6)
+
+png(paste0(file_output, '/PS', '.png'), height =5 , width = 9, units = 'in', res=600)
+p
+dev.off()
 # - Determine week --------------------------------------------------------
 date <- as.Date(x = paste0(Data$year, "-", Data$week, "-1"),
                 format = "%Y-%U-%u")
@@ -99,25 +165,14 @@ p <- ggplot(data=Data_summer, aes(x=week, y=log(CPUE)))+geom_point()+
   stat_smooth( method = "gam", colour="black",size=2, formula = y ~ s(x))
 p
 
-
-# - mean CPUE (kg)/ month/adfg --------------------------------------------------
-Data_1 <- Data_summer %>% group_by(year, week, adfg) %>% summarise(CPUE.mean =mean(CPUE*1000))
-
-p <- ggplot(data=Data_1%>% filter(week %in% c(12:38)), aes(x=week, y=log(CPUE.mean)))+geom_point()+
-  stat_smooth( method = "gam", colour="black",size=2, formula = y ~ s(x)) +theme_bw() 
+# -Plot years --------------------------------------------------------
+p <- ggplot(data=Data_summer, aes(x=year, y=log(CPUE)))+geom_point()+ 
+  stat_smooth( method = "gam", colour="black",size=2, formula = y ~ s(x))
 p
 
-png(paste0(file_output, '/logCPUE=f(week)', '.png'), height =7 , width = 5, units = 'in', res=600)
+png(paste0(file_output, '/logCPUE=f(years_summer)', '.png'), height =7 , width = 5, units = 'in', res=600)
 p
 dev.off()
-
-
-# - Calculate lon lat ADFG --------------------------------------------------
-vect_lat_long <- getLatLong(Data_1$adfg)
-
-colnames(vect_lat_long) <- c("long","lat")
-vect_lat_long<- as_tibble(vect_lat_long)
-Data_adfg <- bind_cols(Data_1,vect_lat_long)
 
 
 # Define covariates -------------------------------------------------------
@@ -125,15 +180,15 @@ Data_adfg <- bind_cols(Data_1,vect_lat_long)
 
 # - Covariate: CP cold/warm years -----------------------------------------
 
-cold <- cbind(c(2006,2007,2008,2009,2010,2011,2012,2013,2017),rep('cold', length(c(2006,2007,2008,2009,2010,2011,2012,2013,2017))))
-warm <- cbind(c(2001,2002,2003,2004,2005,2014,2015,2016,2018), rep('warm', length(c(2001,2002,2003,2004,2005,2014,2015,2016,2018))))
+cold <- cbind(c(2000,2006,2007,2008,2009,2010,2011,2012,2013,2017),rep('cold', length(c(2000,2006,2007,2008,2009,2010,2011,2012,2013,2017))))
+warm <- cbind(c(2001,2002,2003,2004,2005,2014,2015,2016,2018,2019), rep('warm', length(c(2001,2002,2003,2004,2005,2014,2015,2016,2018,2019))))
 #temp <- cbind(c(2000,2001,2017,2019,2020),rep("temp",5))
 Temp <- as_tibble(rbind(cold,warm))
 colnames(Temp)<- c('year','CP')
 Temp$year <- as.numeric(Temp$year)
 
-Data_adfg_1 <- left_join(Data_adfg,Temp)
-Data_adfg_1 <- Data_adfg_1 %>% filter(year>2000, year<2019)
+Data_adfg_1 <- left_join(Data_summer,Temp)
+Data_adfg_1 <- Data_adfg_1 %>% filter(year>1999, year<2020)
 
 # -- Plot CPUE=f(week ,CP)
 p <- ggplot(data=Data_adfg_1%>% filter(week %in% c(12:38)), aes(x=as.numeric(week), y=log(CPUE.mean),color=CP))+ geom_point() + stat_smooth()
@@ -150,10 +205,10 @@ xlims <- c(-179 ,-157)#range(pretty(Database_EBS$Lon))
 ylims <- c(54, 63)#range(pretty(Database_EBS$Lat))
 
 p.Tyears <- ggplot() +
- # geom_point(data=Database_EBS %>% filter(Year==2018),aes(x=Lon, y=Lat),col="grey",size=0.4,shape=3)+
+  # geom_point(data=Database_EBS %>% filter(Year==2018),aes(x=Lon, y=Lat),col="grey",size=0.4,shape=3)+
   geom_polygon(data=map_data("world") , aes(x=long, y = lat, group = group), fill="grey")+
   geom_point( data=Data_adfg_1%>% filter(week %in% c(12:38)), aes(x=long, y=lat, size= log(CPUE.mean+0.1), 
-                                    color=CP,shape=CP),alpha=0.4)  +
+                                                                  color=CP,shape=CP),alpha=0.4)  +
   scale_color_manual(values=c("blue","red")) +
   coord_fixed(xlim = xlims,ylim=ylims)+
   scale_size_continuous(range=c(1,4))+
@@ -186,8 +241,26 @@ Season <- rbind(Early,Int,Late)
 
 Data_adfg_1$week <- as.character(Data_adfg_1$week)
 CPUE_catchability <- right_join(as_tibble(Data_adfg_1), as_tibble(Season))
-colnames(CPUE_catchability) = c("year","week","adfg","CPUE.mean","long","lat","CP","Season")
 
+# - mean CPUE (kg)/ month/adfg --------------------------------------------------
+CPUE_catchability_temp <- CPUE_catchability %>% group_by(year, season, CP,adfg) %>% summarise(CPUE.mean =mean(CPUE*1000))
+
+p <- ggplot(data=Data_1%>% filter(week %in% c(12:38)), aes(x=week, y=log(CPUE.mean)))+geom_point()+
+  stat_smooth( method = "gam", colour="black",size=2, formula = y ~ s(x)) +theme_bw() 
+p
+
+png(paste0(file_output, '/logCPUE=f(week)', '.png'), height =7 , width = 5, units = 'in', res=600)
+p
+dev.off()
+
+# - Calculate lon lat ADFG --------------------------------------------------
+vect_lat_long <- getLatLong(CPUE_catchability_temp$adfg)
+
+colnames(vect_lat_long) <- c("long","lat")
+vect_lat_long<- as_tibble(vect_lat_long)
+CPUE_catchability <- bind_cols(CPUE_catchability_temp,vect_lat_long)
+
+colnames(CPUE_catchability) <- c("year", "Season"   ,  "CP", "adfg","CPUE.mean", "long","lat"  )
 # Save ---------------------------------------------------------------------
 # -------------------------------------------------------------------------
 getwd()
